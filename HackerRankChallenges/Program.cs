596c0b561778f7a000f8050fea06bfeb6d4db412
Vaibhav Lawand <lawand.vaibhav@gmail.com>
2021-11-12 23:50:50 +0530

    Addes skeletop project

---
+++ HackerRankChallenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRankChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
